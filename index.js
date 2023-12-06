8ae411d4130a7522df570ff6446144c4c35ea2bf
juanest20 <148129332+juanest20@users.noreply.github.com>
2023-12-06 16:51:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Abriendo link para más informacion')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click', function () {
+    alert('Igualmente lo sabrás :(')
+})
